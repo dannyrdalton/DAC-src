5b0f06166de228a30dd02605d01e6d9c82f303ad
EC2 Default User <ec2-user@ip-172-31-27-173.us-west-2.compute.internal>
2013-07-08 01:40:53 +0000

    added rails project

---
+++ dacsite/config/initializers/aws.rb
@@ -0,0 +1,4 @@
+AWS.config({
+		:access_key_id => 'AKIAIYCIRKKDOVVLDCWQ',
+		:secret_access_key => 'EJPbASAfx+yFXK/ecizks97CN0XmgqOdXblHX+PM',
+})
---
+++ dacsite/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dacsite::Application.config.secret_key_base = 'c906fbeca5fec9d0075d8fc550d11264a7117877c21c1ae791a84513371285710f49cca2eb4e86b68fa3019248a3d7ee1c1e8a2a28100b6c44769c87b5b81da6'
